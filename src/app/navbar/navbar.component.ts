d5bfb314b72f99df7e7be7588302541e7d35dd79
benneee <benedictiknkeonye@gmail.com>
2019-07-16 22:40:01 +0100

    Username now being displayed; AuthGuard set up

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from './../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'navbar',
@@ -8,12 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   isNavbarCollapsed = true;
+  user: firebase.User;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe(user => this.user = user);
+  }
 
   ngOnInit() {}
 
   logout() {
     this.authService.logoutUser();
+    this.afAuth.auth.signOut();
   }
 }
