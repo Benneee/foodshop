e5487ec16df0d7d29deac15cb6272e72b13f94a2
benneee <benedictiknkeonye@gmail.com>
2019-07-31 13:04:07 +0100

    Changed product table type to angular5-datatable

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,8 @@
-import { Product } from './../../product.model';
+import { Product } from '../../models/product.model';
 import { ProductService } from './../../providers/product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { DataTableResource } from 'angular5-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,15 +13,37 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
+
   constructor(private productService: ProductService) {
-  this.subscription = this.productService.getAll()
-      .subscribe(products => this.filteredProducts = this.products = products);
+    this.subscription = this.productService.getAll().subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    });
   }
 
   ngOnInit() {}
 
   filter(value: string) {
-    this.filteredProducts = (value) ? this.products.filter(p => p.title.toLowerCase().includes(value.toLowerCase())) : this.products;
+    this.filteredProducts = value
+      ? this.products.filter(p =>
+          p.title.toLowerCase().includes(value.toLowerCase())
+        )
+      : this.products;
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 }).then(items => (this.items = items));
+
+    this.tableResource.count().then(count => (this.itemCount = count));
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+    this.tableResource.query(params).then(items => (this.items = items));
   }
 
   /**
@@ -29,6 +52,6 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
    */
 
   ngOnDestroy() {
-    this.subscription.unsubscribe()
+    this.subscription.unsubscribe();
   }
 }
