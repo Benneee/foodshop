bf560d127f2bf3c70f001db10db70b60dce713c8
benneee <benedictiknkeonye@gmail.com>
2019-08-13 00:21:51 +0100

    Added validation to the shipping form

--- src/app/shipping-form/shipping-form.component.ts
+++ src/app/shipping-form/shipping-form.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Order } from '../models/order.model';
-import { Subscription } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
-import { ShoppingCartService } from '../providers/shopping-cart.service';
-import { OrderService } from '../order.service';
-import { Router } from '@angular/router';
 import { ShoppingCart } from '../models/shopping-cart.model';
+import { OrderService } from './../order.service';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Order } from '../models/order.model';
 
 @Component({
   selector: 'shipping-form',
   templateUrl: './shipping-form.component.html',
   styleUrls: ['./shipping-form.component.css']
 })
-export class ShippingFormComponent implements OnInit {
+export class ShippingFormComponent implements OnInit, OnDestroy {
+  @Input('cart') cart: ShoppingCart;
   shipping = {};
   userSubscription: Subscription;
   userId: string;
-  @Input() cart: ShoppingCart;
+
   constructor(
+    private router: Router,
     private authService: AuthService,
-    private orderService: OrderService,
-    private router: Router
+    private orderService: OrderService
   ) {}
+
   ngOnInit() {
     this.userSubscription = this.authService.user$.subscribe(
       user => (this.userId = user.uid)
@@ -33,10 +33,9 @@ export class ShippingFormComponent implements OnInit {
     this.userSubscription.unsubscribe();
   }
 
-  async placeOrder(shippingForm: NgForm) {
-    let order = new Order(this.userId, shippingForm.value, this.cart);
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
     let result = await this.orderService.placeOrder(order);
-    console.log(order);
     // $key is used when you read a node off/from firebase
     // key is used when you store something in firebase, e.g, we stored the user's ID...
     this.router.navigate(['/order-success', result.key]);
