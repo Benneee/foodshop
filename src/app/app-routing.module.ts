f51fa21ec74a8efbb3d9b889ea8dcd7c8cb1447d
benneee <benedictiknkeonye@gmail.com>
2019-08-18 06:30:41 +0100

    Completed core module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './core/components/login/login.component';
-import { SignupComponent } from './core/components/signup/signup.component';
 import { ProductsComponent } from './shopping/components/products/products.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
-  { path: 'spinner', component: SpinnerComponent },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'spinner', component: SpinnerComponent }
 ];
 
 @NgModule({
