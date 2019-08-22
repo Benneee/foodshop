6981553a53be7c9d25d2356085effd18c5304139
benneee <benedictiknkeonye@gmail.com>
2019-08-22 22:54:43 +0100

    Updated UX and added loaders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
